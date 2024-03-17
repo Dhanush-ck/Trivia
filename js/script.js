ed122d1af2587acc3043631d5969e86f93da212a
Dhanush CK <dhanushck01@gmail.com>
2024-03-17 22:32:21 +0530

    Added Questions

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const questions = [
+    {
+        question: "Who is the father of Computer?", 
+        answers: [
+            { text: "Charles Babbage", correct: true},
+            { text: "Alan Turing", correct: false},
+            { text: "Konrad Zuse", correct: false},
+            { text: "Ada Lovelace", correct: false},
+        ] 
+    },
+    {
+        question: "How many Bits make one Byte?", 
+        answers: [
+            { text: "16 Bits", correct: false},
+            { text: "32 Bits ", correct: false},
+            { text: "8 Bits", correct: true},
+            { text: "64 Bits", correct: false},
+        ] 
+    },
+    {
+        question: "Which electronic component was used in the first generation of computers?", 
+        answers: [
+            { text: "Vaccum Tube", correct: true},
+            { text: "Transistor", correct: false},
+            { text: "Microprocessor", correct: false},
+            { text: "Integrated Circuit(IC)", correct: false},
+        ] 
+    },
+    {
+        question: "What is the full form of RAM?", 
+        answers: [
+            { text: "Random Applicable Memory", correct: false},
+            { text: "Read Access Memory", correct: false},
+            { text: "Read Applicable Memory", correct: false},
+            { text: "Random Acces Memory", correct: true},
+        ] 
+    },
+    {
+        question: "An address given to a computer connected to a network is called ?", 
+        answers: [
+            { text: "Local address", correct: false},
+            { text: "Localhost", correct: false},
+            { text: "Network address", correct: false},
+            { text: "IP adderss", correct: true},
+        ] 
+    },
+    {
+        question: "C, Java, C++ and PHP are examples for ?", 
+        answers: [
+            { text: "Software Types", correct: false},
+            { text: "Firmware", correct: false},
+            { text: "Programming Languages", correct: true},
+            { text: "Programming Translators", correct: false},
+        ] 
+    }
+]
